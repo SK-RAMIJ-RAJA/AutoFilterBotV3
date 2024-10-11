0e1c2691a681e31dc3307ff24c652c02dc1ec35a
SK-RAMIJ-RAJA <akabusiness010@gmail.com>
2024-10-11 11:02:13 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -3,13 +3,18 @@ import logging
 from pyrogram import Client, filters
 from pyrogram import StopPropagation
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from Config import START_MSG, CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, TUTORIAL, BROADCAST_CHANNEL, DB_URL, SESSION, ADMIN_ID    
-from LuciferMoringstar_Robot.Utils import Media, get_file_details 
+from Config import (
+    START_MSG, CHANNELS, ADMINS, AUTH_CHANNEL,
+    CUSTOM_FILE_CAPTION, TUTORIAL, BROADCAST_CHANNEL,
+    DB_URL, SESSION, ADMIN_ID
+)
+from LuciferMoringstar_Robot.Utils import Media, get_file_details
 from LuciferMoringstar_Robot.Broadcast import broadcast
 from LuciferMoringstar_Robot import ABOUT
 from LuciferMoringstar_Robot.Channel import handle_user_status
 from Database import Database
 from pyrogram.errors import UserNotParticipant
+
 logger = logging.getLogger(__name__)
 
 LOG_CHANNEL = BROADCAST_CHANNEL
@@ -72,27 +77,34 @@ async def start(bot, message):
             filedetails = await get_file_details(file_id)
             for files in filedetails:
                 title = files.file_name
-                size=files.file_size
-                f_caption=files.caption
+                size = files.file_size
+                f_caption = files.caption
+                
+                # Add custom caption with file name and size
                 if CUSTOM_FILE_CAPTION:
                     try:
-                        f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+                        f_caption = CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
                     except Exception as e:
                         print(e)
-                        f_caption=f_caption
+                        f_caption = f_caption
+                
                 if f_caption is None:
                     f_caption = f"{files.file_name}"
+                
+                # Creating buttons
                 buttons = [
                     [
                         InlineKeyboardButton('🎖 DEPLOY YOURS 🎖', url=f'{TUTORIAL}')
                     ]
-                    ]
+                ]
+
+                # Send the file with caption
                 await bot.send_cached_media(
                     chat_id=message.from_user.id,
                     file_id=file_id,
-                    caption=f_caption,
+                    caption=f"{f_caption} \n\n🏷️ Title: _{title}_ \n📏 Size: _{size}_",  # Adding title and size here
                     reply_markup=InlineKeyboardMarkup(buttons)
-                    )
+                )
         except Exception as err:
             await message.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
     elif len(message.command) > 1 and message.command[1] == 'subscribe':
@@ -115,10 +127,10 @@ async def start(bot, message):
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[
-                InlineKeyboardButton("Search Here", switch_inline_query_current_chat='')
-                ],[
-                InlineKeyboardButton("Help", callback_data="help"),
-                InlineKeyboardButton("About", callback_data="about")
+                    InlineKeyboardButton("Search Here", switch_inline_query_current_chat='')
+                ], [
+                    InlineKeyboardButton("Help", callback_data="help"),
+                    InlineKeyboardButton("About", callback_data="about")
                 ]]
             )
         )
@@ -153,7 +165,6 @@ async def channel_info(bot, message):
         await message.reply_document(file)
         os.remove(file)
 
-
 @Client.on_message(filters.private & filters.command("broadcast"))
 async def broadcast_handler_open(_, m):
     if m.from_user.id not in ADMIN_ID:
@@ -163,7 +174,6 @@ async def broadcast_handler_open(_, m):
         await m.delete()
     else:
         await broadcast(m, db)
-
 
 @Client.on_message(filters.private & filters.command("stats"))
 async def sts(c, m):
@@ -175,7 +185,6 @@ async def sts(c, m):
         parse_mode="Markdown",
         quote=True
     )
-
 
 @Client.on_message(filters.private & filters.command("ban_user"))
 async def ban(c, m):
@@ -216,7 +225,6 @@ async def ban(c, m):
             quote=True
         )
 
-
 @Client.on_message(filters.private & filters.command("unban_user"))
 async def unban(c, m):
     if m.from_user.id not in ADMIN_ID:
@@ -231,107 +239,4 @@ async def unban(c, m):
 
     try:
         user_id = int(m.command[1])
-        unban_log_text = f"Unbanning user 🤪 {user_id}"
-
-        try:
-            await c.send_message(user_id, f"Your ban was lifted!")
-            unban_log_text += "\n\n✅ User notified successfully! ✅"
-        except BaseException:
-            traceback.print_exc()
-            unban_log_text += (
-                f"\n\n⚠️ User notification failed! ⚠️\n\n`{traceback.format_exc()}`"
-            )
-        await db.remove_ban(user_id)
-        print(unban_log_text)
-        await m.reply_text(unban_log_text, quote=True)
-    except BaseException:
-        traceback.print_exc()
-        await m.reply_text(
-            f"⚠️ Error occoured ⚠️! Traceback given below\n\n`{traceback.format_exc()}`",
-            quote=True,
-        )
-
-
-@Client.on_message(filters.private & filters.command("banned_users"))
-async def _banned_usrs(c, m):
-    if m.from_user.id not in ADMIN_ID:
-        await m.delete()
-        return
-    all_banned_users = await db.get_all_banned_users()
-    banned_usr_count = 0
-    text = ""
-    async for banned_user in all_banned_users:
-        user_id = banned_user["id"]
-        ban_duration = banned_user["ban_status"]["ban_duration"]
-        banned_on = banned_user["ban_status"]["banned_on"]
-        ban_reason = banned_user["ban_status"]["ban_reason"]
-        banned_usr_count += 1
-        text += f"> **User_id**: `{user_id}`, **Ban Duration**: `{ban_duration}`, **Banned on**: `{banned_on}`, **Reason**: `{ban_reason}`\n\n"
-    reply_text = f"Total banned user(s) 🤭: `{banned_usr_count}`\n\n{text}"
-    if len(reply_text) > 4096:
-        with open("banned-users.txt", "w") as f:
-            f.write(reply_text)
-        await m.reply_document("banned-users.txt", True)
-        os.remove("banned-users.txt")
-        return
-    await m.reply_text(reply_text, True)
-
-
-
-@Client.on_message(filters.command('total') & filters.user(ADMINS))
-async def total(bot, message):
-    """Show total files in database"""
-    msg = await message.reply("Processing...⏳", quote=True)
-    try:
-        total = await Media.count_documents()
-        await msg.edit(f'📁 Saved files: {total}')
-    except Exception as e:
-        logger.exception('Failed to check total files')
-        await msg.edit(f'Error: {e}')
-
-
-@Client.on_message(filters.command('logger') & filters.user(ADMINS))
-async def log_file(bot, message):
-    """Send log file"""
-    try:
-        await message.reply_document('TelegramBot.log')
-    except Exception as e:
-        await message.reply(str(e))
-
-
-@Client.on_message(filters.command('delete') & filters.user(ADMINS))
-async def delete(bot, message):
-    """Delete file from database"""
-    reply = message.reply_to_message
-    if reply and reply.media:
-        msg = await message.reply("Processing...⏳", quote=True)
-    else:
-        await message.reply('Reply to file with /delete which you want to delete', quote=True)
-        return
-
-    for file_type in ("document", "video", "audio"):
-        media = getattr(reply, file_type, None)
-        if media is not None:
-            break
-    else:
-        await msg.edit('This is not supported file format')
-        return
-
-    result = await Media.collection.delete_one({
-        'file_name': media.file_name,
-        'file_size': media.file_size,
-        'mime_type': media.mime_type
-    })
-    if result.deleted_count:
-        await msg.edit('File is successfully deleted from database')
-    else:
-        await msg.edit('File not found in database')
-@Client.on_message(filters.command('about'))
-async def bot_info(bot, message):
-    buttons = [
-        [
-            
-            InlineKeyboardButton('Deploy Video', url=f'{TUTORIAL}')
-        ]
-        ]
-    await message.reply(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+        unban_log_text = f"Unbanning user 🤪 {user_id
